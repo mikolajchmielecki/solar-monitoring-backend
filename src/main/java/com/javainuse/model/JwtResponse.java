f5564e1d2dcc258631884200030de1dfbdecb75f
mchmielecki_252723 <mchmielecki_252723@e-science.pl>
2022-09-13 18:06:51 +0200

    change getters and setter to lombok annotations

--- src/main/java/com/javainuse/model/JwtResponse.java
+++ src/main/java/com/javainuse/model/JwtResponse.java
@@ -1,17 +1,15 @@
 package com.javainuse.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.io.Serializable;
 
+@Getter
+@AllArgsConstructor
 public class JwtResponse implements Serializable {
 
 	private static final long serialVersionUID = -8091879091924046844L;
-	private final String jwttoken;
+	private final String token;
 
-	public JwtResponse(String jwttoken) {
-		this.jwttoken = jwttoken;
-	}
-
-	public String getToken() {
-		return this.jwttoken;
-	}
 }
