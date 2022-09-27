d6f512965c6cdc12d98a48b5a2273e7e2b2e9d41
Mikolaj Chmielecki <mikolaj.chmielecki.ext@siemens.com>
2022-09-27 21:51:20 +0200

    problem with headless mode

--- src/main/java/pl/edu/pwr/solarmonitoring/exchange/energa/EnergaExchange.java
+++ src/main/java/pl/edu/pwr/solarmonitoring/exchange/energa/EnergaExchange.java
@@ -5,11 +5,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 import pl.edu.pwr.solarmonitoring.model.User;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
 @Component
 @Slf4j
@@ -17,33 +23,64 @@ public class EnergaExchange {
 
     private static final String URL = "https://mojlicznik.energa-operator.pl/";
 
-    public EnergaData getEnergaData(User user) throws InterruptedException {
-    }
-
-        public EnergaData getEnergaData(User user) throws InterruptedException {
+    public EnergaData getEnergaData(User user) {
         log.debug("getEnergyData for " + user);
         if (user.getCounter() != null) {
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\mikol\\OneDrive - Politechnika Wroclawska\\Desktop\\chromedriver.exe");
-            WebDriver webDriver = new ChromeDriver();
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\chmimiko\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--start-maximized");
+            options.addArguments("--headless");
+            WebDriver webDriver = new ChromeDriver(options);
             webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            webDriver.get(URL);
-            webDriver.findElement(By.xpath("//*[@id=\"loginRadio\"]")).click();
-            webDriver.findElement(By.xpath("//*[@id=\"j_username\"]")).sendKeys(user.getCounter().getLogin());
-            webDriver.findElement(By.xpath("//*[@id=\"j_password\"]")).sendKeys(user.getCounter().getPassword());
-            webDriver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[4]/button")).click();
+            login(webDriver, user);
+
+            // go to energy view
             webDriver.findElement(By.xpath("//*[@id=\"mainMenu\"]/ul/li[2]/a")).click();
 
-            Actions action = new Actions(webDriver);
-            WebElement bar = webDriver.findElement(By.xpath("/html/body/*[name()='svg'][1]/*[name()='rect' and @fill='#bce05c'][1]"));
-            action.moveToElement(bar).perform();
+            int currentMonth = LocalDate.now().getMonthValue();
+            Double chargeEnergy = getEnergyInCurrentMonth(webDriver, currentMonth, 4);
 
-            Thread.sleep(10000L);
-            return new EnergaData(0D, 1D);
+            // go to remit energy
+            new Select(webDriver.findElement(By.xpath("//*[@id=\"meterobjecttochart\"]"))).selectByValue("A-");
+            Double remitEnergy = getEnergyInCurrentMonth(webDriver, currentMonth, 5);
+
+            webDriver.quit();
+            return new EnergaData(chargeEnergy, remitEnergy);
         } else {
             String msg = "User hasn't set counter credentials.";
             log.error(msg);
             throw new RuntimeException(msg);
         }
+    }
+
+    /**
+     *
+     * @param webDriver
+     * @param currentMonth - indexed from 1
+     * @return
+     */
+    private Double getEnergyInCurrentMonth(WebDriver webDriver, int currentMonth, int index) {
+        Actions action = new Actions(webDriver);
+        WebElement bar = webDriver.findElement(By.xpath(String.format("/html/body/*[name()='svg'][1]/*[name()='rect' and @fill='#bce05c'][%s]", currentMonth)));
+        action.moveToElement(bar).perform();
+
+        By valueBoxXpath = By.xpath(String.format("/html/body/*[name()='svg'][%s]/*[name()='text'][2]/*[name()='tspan']", index));
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(valueBoxXpath));
+        WebElement valueBox = webDriver.findElement(valueBoxXpath);
+        String value = valueBox.getText().replaceAll("[^0-9.]+", " ").trim().split(" ")[0];
+        return Double.parseDouble(value);
+
+    }
+
+    private void login(WebDriver webDriver, User user) {
+        webDriver.get(URL);
+        webDriver.findElement(By.xpath("//*[@id=\"loginRadio\"]")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"j_username\"]")).sendKeys(user.getCounter().getLogin());
+        webDriver.findElement(By.xpath("//*[@id=\"j_password\"]")).sendKeys(user.getCounter().getPassword());
+        webDriver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[4]/button")).click();
     }
 
 }
