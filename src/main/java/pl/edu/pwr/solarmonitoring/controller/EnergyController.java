961ca2032d86b6421f81638a7eee0abeac380bf2
Mikolaj Chmielecki <mikolaj.chmielecki.ext@siemens.com>
2022-09-28 21:08:43 +0200

    energa exchange final

--- src/main/java/pl/edu/pwr/solarmonitoring/controller/EnergyController.java
+++ src/main/java/pl/edu/pwr/solarmonitoring/controller/EnergyController.java
@@ -5,9 +5,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.pwr.solarmonitoring.exchange.energa.EnergaData;
 import pl.edu.pwr.solarmonitoring.exchange.energa.EnergaExchange;
 import pl.edu.pwr.solarmonitoring.model.User;
 import pl.edu.pwr.solarmonitoring.utils.UserUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/energy")
@@ -16,9 +21,4 @@ public class EnergyController {
 
     private final EnergaExchange energaExchange;
 
-    @GetMapping("energa")
-    public String getEnerga(Authentication authentication) throws InterruptedException {
-        User user = UserUtils.fromAuthentication(authentication);
-        return energaExchange.getEnergaData(user).toString();
-    }
 }
