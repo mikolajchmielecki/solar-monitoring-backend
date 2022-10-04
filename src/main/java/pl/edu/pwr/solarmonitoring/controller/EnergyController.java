22718a7931c747197d28b4b55092fc37bbe3a27f
Mikołaj Chmielecki <58306788+mikolajchmielecki@users.noreply.github.com>
2022-10-04 17:34:29 +0200

    Energa (#7)
    * first commit
    
    * problem with headless mode
    
    * problem with headless mode
    
    * energa exchange final
    
    * energa exchange final
    
    Co-authored-by: mchmielecki_252723 <mchmielecki_252723@e-science.pl>
    Co-authored-by: Mikolaj Chmielecki <mikolaj.chmielecki.ext@siemens.com>

--- src/main/java/pl/edu/pwr/solarmonitoring/controller/EnergyController.java
+++ src/main/java/pl/edu/pwr/solarmonitoring/controller/EnergyController.java
@@ -1,12 +1,13 @@
 package pl.edu.pwr.solarmonitoring.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pwr.solarmonitoring.exchange.energa.EnergaExchange;
-import pl.edu.pwr.solarmonitoring.model.User;
 import pl.edu.pwr.solarmonitoring.utils.UserUtils;
 
 @RestController
@@ -16,9 +17,9 @@ public class EnergyController {
 
     private final EnergaExchange energaExchange;
 
-    @GetMapping("energa")
-    public String getEnerga(Authentication authentication) throws InterruptedException {
-        User user = UserUtils.fromAuthentication(authentication);
-        return energaExchange.getEnergaData(user).toString();
+    @GetMapping("/before")
+    public ResponseEntity getEnergyInBeforeMonth(Authentication authentication) {
+        return new ResponseEntity(energaExchange.getEnergaDataForBeforeMonth(UserUtils.fromAuthentication(authentication)), HttpStatus.OK);
     }
+
 }
--- src/main/java/pl/edu/pwr/solarmonitoring/exchange/energa/EnergaExchange.java
+++ src/main/java/pl/edu/pwr/solarmonitoring/exchange/energa/EnergaExchange.java
@@ -6,43 +6,62 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.security.core.parameters.P;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.edu.pwr.solarmonitoring.model.User;
 
+import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Collection;
 
 @Component
 @Slf4j
 public class EnergaExchange {
 
-    private static final String URL = "https://mojlicznik.energa-operator.pl/";
+    private static final String ENERGA_URL = "https://mojlicznik.energa-operator.pl/";
 
-    public EnergaData getEnergaData(User user) {
+    @Value("${selenium.grid.url}")
+    private String GRID_URL;
+
+
+    public EnergaData getEnergaDataForBeforeMonth(User user) {
         log.debug("getEnergyData for " + user);
         if (user.getCounter() != null) {
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\chmimiko\\Downloads\\chromedriver_win32\\chromedriver.exe");
-            WebDriver webDriver = new ChromeDriver();
-            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            login(webDriver, user);
+            WebDriver webDriver = null;
+            try {
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\chmimiko\\Downloads\\chromedriver_win32\\chromedriver.exe");
+                FirefoxOptions options = new FirefoxOptions();
+                webDriver = new RemoteWebDriver(new URL(GRID_URL), options);
+                webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                login(webDriver, user);
 
-            // go to energy view
-            webDriver.findElement(By.xpath("//*[@id=\"mainMenu\"]/ul/li[2]/a")).click();
+                // go to energy view
+                webDriver.findElement(By.xpath("//*[@id=\"mainMenu\"]/ul/li[2]/a")).click();
 
-            int currentMonth = LocalDate.now().getMonthValue();
-            Double chargeEnergy = getEnergyInCurrentMonth(webDriver, currentMonth, 4);
+                int currentMonth = LocalDate.now().getMonthValue() - 1;
+                Double chargeEnergy = getEnergyInCurrentMonth(webDriver, currentMonth, 4);
 
-            // go to remit energy
-            new Select(webDriver.findElement(By.xpath("//*[@id=\"meterobjecttochart\"]"))).selectByValue("A-");
-            Double remitEnergy = getEnergyInCurrentMonth(webDriver, currentMonth, 5);
+                // go to remit energy
+                new Select(webDriver.findElement(By.xpath("//*[@id=\"meterobjecttochart\"]"))).selectByValue("A-");
+                Double remitEnergy = getEnergyInCurrentMonth(webDriver, currentMonth, 5);
 
-            webDriver.quit();
-            return new EnergaData(chargeEnergy, remitEnergy);
+                webDriver.quit();
+                return new EnergaData(chargeEnergy, remitEnergy);
+            } catch (Exception e) {
+                String msg = "Error during communication with Energa website";
+                log.error(e.getMessage());
+                if (webDriver != null) {
+                    webDriver.quit();
+                }
+                throw new RuntimeException(msg, e.getCause());
+            }
         } else {
             String msg = "User hasn't set counter credentials.";
             log.error(msg);
@@ -53,26 +72,34 @@ public class EnergaExchange {
     /**
      *
      * @param webDriver
-     * @param currentMonth - indexed from 1
+     * @param currentMonth - indexed from 0, if 0 change year to before
      * @return
      */
     private Double getEnergyInCurrentMonth(WebDriver webDriver, int currentMonth, int index) {
+        if (currentMonth == 0) {
+            webDriver.findElement(By.xpath("//*[@id=\"buttonPrev\"]")).click();
+            currentMonth = 12;
+        }
         Actions action = new Actions(webDriver);
         WebElement bar = webDriver.findElement(By.xpath(String.format("/html/body/*[name()='svg'][1]/*[name()='rect' and @fill='#bce05c'][%s]", currentMonth)));
         action.moveToElement(bar).perform();
 
         By valueBoxXpath = By.xpath(String.format("/html/body/*[name()='svg'][%s]/*[name()='text'][2]/*[name()='tspan']", index));
-
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.presenceOfElementLocated(valueBoxXpath));
         WebElement valueBox = webDriver.findElement(valueBoxXpath);
+
+        if (currentMonth == 12) {
+            webDriver.findElement(By.xpath("//*[@id=\"buttonNext\"]")).click();
+        }
+
         String value = valueBox.getText().replaceAll("[^0-9.]+", " ").trim().split(" ")[0];
         return Double.parseDouble(value);
 
     }
 
     private void login(WebDriver webDriver, User user) {
-        webDriver.get(URL);
+        webDriver.get(ENERGA_URL);
         webDriver.findElement(By.xpath("//*[@id=\"loginRadio\"]")).click();
         webDriver.findElement(By.xpath("//*[@id=\"j_username\"]")).sendKeys(user.getCounter().getLogin());
         webDriver.findElement(By.xpath("//*[@id=\"j_password\"]")).sendKeys(user.getCounter().getPassword());
