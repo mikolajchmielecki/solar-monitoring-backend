80e1cd6bdcd0d3a3cf7a5583c35a5a9079e34427
mchmielecki_252723 <mchmielecki_252723@e-science.pl>
2022-12-05 15:51:29 +0100

    license

--- src/main/java/pl/edu/pwr/solarmonitoring/custom/AfterStartup.java
+++ src/main/java/pl/edu/pwr/solarmonitoring/custom/AfterStartup.java
@@ -1,3 +1,19 @@
+/*
+ * SolarMonitoring
+ * Copyright (C) 2022 Mikołaj Chmielecki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * LICENSE file in root directory contains a copy of the GNU General Public License.
+ */
+
 package pl.edu.pwr.solarmonitoring.custom;
 
 import lombok.RequiredArgsConstructor;
